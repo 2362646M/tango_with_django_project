7e884ff00040dbd8df72aa2fd0fd847c6e99da77
Fin McCallum <2362646M@student.gla.ac.uk>
2020-01-17 17:07:09 +0000

    added about page and url on index page

--- rango/views.py
+++ rango/views.py
@@ -3,4 +3,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango  says hey there partner!")
+    html = '<html><body><a href="about">Rango says hey there partner</a></body></html>'
+    return HttpResponse(html)
+
+def about(request):
+    html = '<html><body><a href="index">Rango says here is the about page.</a></body></html>'
+    return HttpResponse(html)
